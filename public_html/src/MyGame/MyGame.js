c352caf93a83265e82254edf7b1287f383e354fd
Jedek <jeroen@jedeksvault.nl>
2016-12-29 15:12:12 +0100

    Added a Renderables generation function in client code to abstract the Renderables process.
    Client tracks all renderables in array and loops through it for rendering

--- public_html/src/MyGame/MyGame.js
+++ public_html/src/MyGame/MyGame.js
@@ -4,6 +4,8 @@ function MyGame(htmlCanvasID) {
     // Step A: Initialize the webGL Context and the VertexBuffer
     gEngine.Core.initializeWebGL(htmlCanvasID);
     
+    this.mRenderables = [];
+    
     // Step B: Setup the camera
     this.mCamera = new Camera(
             vec2.fromValues(20, 60), // center of the World Coordinate System
@@ -15,19 +17,13 @@ function MyGame(htmlCanvasID) {
             "src/GLSLShaders/SimpleVS.glsl", // Path to the VertexShader
             "src/GLSLShaders/SimpleFS.glsl"); // Path to the FragmentShader
     
-    // Step C: Create the Renderable objects
-    this.mBlueSq = new Renderable(this.mConstColorShader);
-    this.mBlueSq.setColor([0.25,0.25,0.95,1]);
-    this.mRedSq = new Renderable(this.mConstColorShader);
-    this.mRedSq.setColor([1, 0.25, 0.25, 1]);
-    this.mTLSq = new Renderable(this.mConstColorShader);
-    this.mTLSq.setColor([0.9, 0.1, 0.1, 1]); // Top-left shows red
-    this.mTRSq = new Renderable(this.mConstColorShader);
-    this.mTRSq.setColor([0.1, 0.9, 0.1, 1]); // Top-right shows green
-    this.mBRSq = new Renderable(this.mConstColorShader);
-    this.mBRSq.setColor([0.1, 0.1, 0.9, 1]); // Top-right shows green
-    this.mBLSq = new Renderable(this.mConstColorShader);
-    this.mBLSq.setColor([0.1, 0.1, 0.1, 1]); // Top-right shows green
+    // Step C: Generate the Renderable objects and store them in an array
+    this.generateRenderable([0.25,0.25,0.95,1], [20, 60], [5,5]); // Big blue sqare
+    this.generateRenderable([0.9, 0.1, 0.1, 1], [20, 60], [2,2]); // Little red square
+    this.generateRenderable([0.9, 0.1, 0.1, 1], [10, 65]); // Top left
+    this.generateRenderable([0.1, 0.9, 0.1, 1], [30, 65]); // Top right
+    this.generateRenderable([0.1, 0.1, 0.9, 1], [30, 55]); // bottom left
+    this.generateRenderable([0.1, 0.1, 0.1, 1], [10, 55]); // bottom right
     
     // Step E: Clear the canvas
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1]); // Clear the canvas
@@ -35,32 +31,24 @@ function MyGame(htmlCanvasID) {
     // Step F: Start drawing by activating the camera
     this.mCamera.setupViewProjection();
     var vpMatrix = this.mCamera.getVPMatrix();
-  
-    // Step G: Draw the blue square
-    this.mBlueSq.getXform().setPosition(20, 60); // to show alternative to setPosition
-    this.mBlueSq.getXform().setRotationInRad(0.2); // This is in Degree
-    this.mBlueSq.getXform().setSize(5,5); // To show alternative to setSize
-    this.mBlueSq.draw(vpMatrix);
-    
-    // Step H: Draw with the red shader
-    // Center red square
-    this.mRedSq.getXform().setPosition(20, 60);
-    this.mRedSq.getXform().setSize(2,2);
-    this.mRedSq.draw(vpMatrix);
-    
-    // Top left
-    this.mTLSq.getXform().setPosition(10,65);
-    this.mTLSq.draw(vpMatrix);
-    
-    // Top right
-    this.mTRSq.getXform().setPosition(30,65);
-    this.mTRSq.draw(vpMatrix);
-    
-    // Bottom left
-    this.mBLSq.getXform().setPosition(30,55);
-    this.mBLSq.draw(vpMatrix);
-    
-    // Bottom right
-    this.mBRSq.getXform().setPosition(10,55);
-    this.mBRSq.draw(vpMatrix);
+
+    // Step G: Run through the renderables array and draw away!
+    for(var i=0; i < this.mRenderables.length; i++) {
+        this.mRenderables[i].draw(vpMatrix);
+    }
 }
+
+MyGame.prototype.generateRenderable = function(color, position, size) {
+    var mRenderable = new Renderable(this.mConstColorShader);
+    mRenderable.setColor(color);
+    
+    if(position) {
+        mRenderable.getXform().setPosition(position[0], position[1]);
+    }
+    
+    if(size) {
+        mRenderable.getXform().setSize(size[0], size[1]);
+    }
+    
+    this.mRenderables.push(mRenderable);
+};
