7f15ea1b3c298bc35aea5c13306753354af79bd1
Jedek <jeroen@jedeksvault.nl>
2016-12-29 17:18:01 +0100

    Added a Game Loop that continues to run and render accordingly.
    There is a bug where the white and red square are not moving.

--- public_html/src/MyGame/MyGame.js
+++ public_html/src/MyGame/MyGame.js
@@ -1,66 +1,89 @@
-"use strict";
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
 
 function MyGame(htmlCanvasID) {
-    // Step A: Initialize the webGL Context and the VertexBuffer
+    // variables of the constant color shader
+    this.mConstColorShader = null;
+
+    // variables for the squares
+    this.mWhiteSq = null;        // these are the Renderable objects
+    this.mRedSq = null;
+
+    // The camera to view the scene
+    this.mCamera = null;
+
+    // Initialize the webGL Context
     gEngine.Core.initializeWebGL(htmlCanvasID);
-    
-    // Step B: Setup the camera
-    this.mCamera = new Camera(
-            vec2.fromValues(20, 60), // center of the World Coordinate System
-            20, // Width of the WC
-            [20, 40, 600, 300]); // viewport (orgX, orgY, width, height)
-    
-    // Step C: Create the shader
-    this.mConstColorShader = new SimpleShader(
-            "src/GLSLShaders/SimpleVS.glsl", // Path to the VertexShader
-            "src/GLSLShaders/SimpleFS.glsl"); // Path to the FragmentShader
-    
-    // Step C: Create the Renderable objects
-    this.mBlueSq = new Renderable(this.mConstColorShader);
-    this.mBlueSq.setColor([0.25,0.25,0.95,1]);
-    this.mRedSq = new Renderable(this.mConstColorShader);
-    this.mRedSq.setColor([1, 0.25, 0.25, 1]);
-    this.mTLSq = new Renderable(this.mConstColorShader);
-    this.mTLSq.setColor([0.9, 0.1, 0.1, 1]); // Top-left shows red
-    this.mTRSq = new Renderable(this.mConstColorShader);
-    this.mTRSq.setColor([0.1, 0.9, 0.1, 1]); // Top-right shows green
-    this.mBRSq = new Renderable(this.mConstColorShader);
-    this.mBRSq.setColor([0.1, 0.1, 0.9, 1]); // Top-right shows green
-    this.mBLSq = new Renderable(this.mConstColorShader);
-    this.mBLSq.setColor([0.1, 0.1, 0.1, 1]); // Top-right shows green
-    
-    // Step E: Clear the canvas
-    gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1]); // Clear the canvas
-    
-    // Step F: Start drawing by activating the camera
-    this.mCamera.setupViewProjection();
-    var vpMatrix = this.mCamera.getVPMatrix();
-  
-    // Step G: Draw the blue square
-    this.mBlueSq.getXform().setPosition(20, 60); // to show alternative to setPosition
-    this.mBlueSq.getXform().setRotationInRad(0.2); // This is in Degree
-    this.mBlueSq.getXform().setSize(5,5); // To show alternative to setSize
-    this.mBlueSq.draw(vpMatrix);
-    
-    // Step H: Draw with the red shader
-    // Center red square
-    this.mRedSq.getXform().setPosition(20, 60);
-    this.mRedSq.getXform().setSize(2,2);
-    this.mRedSq.draw(vpMatrix);
-    
-    // Top left
-    this.mTLSq.getXform().setPosition(10,65);
-    this.mTLSq.draw(vpMatrix);
-    
-    // Top right
-    this.mTRSq.getXform().setPosition(30,65);
-    this.mTRSq.draw(vpMatrix);
-    
-    // Bottom left
-    this.mBLSq.getXform().setPosition(30,55);
-    this.mBLSq.draw(vpMatrix);
-    
-    // Bottom right
-    this.mBRSq.getXform().setPosition(10,55);
-    this.mBRSq.draw(vpMatrix);
+
+    // Initialize the game
+    this.initialize();
 }
+
+MyGame.prototype.initialize = function () {
+    // Step A: set up the cameras
+    this.mCamera = new Camera(
+        vec2.fromValues(20, 60),   // position of the camera
+        20,                        // width of camera
+        [20, 40, 600, 300]);       // viewport (orgX, orgY, width, height)
+
+    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+            // sets the background to dark gray
+
+    // Step  B: create the shader
+    this.mConstColorShader = new SimpleShader(
+        "src/GLSLShaders/SimpleVS.glsl",      // Path to the VertexShader 
+        "src/GLSLShaders/SimpleFS.glsl");    // Path to the Simple FragmentShader    
+
+    // Step  C: Create the Renderable objects:
+    this.mWhiteSq = new Renderable(this.mConstColorShader);
+    this.mWhiteSq.setColor([1, 1, 1, 1]);
+    this.mRedSq = new Renderable(this.mConstColorShader);
+    this.mRedSq.setColor([1, 0, 0, 1]);
+
+    // Step  D: Initialize the white Renderable object: centred, 5x5, rotated
+    this.mWhiteSq.getXform().setPosition(20, 60);
+    this.mWhiteSq.getXform().setRotationInRad(0.2); // In Radians
+    this.mWhiteSq.getXform().setSize(5, 5);
+
+    // Step  E: Initialize the red Renderable object: centered 2x2
+    this.mRedSq.getXform().setPosition(20, 60);
+    this.mRedSq.getXform().setSize(2, 2);
+
+    // Step F: Start the game loop running
+    gEngine.GameLoop.start(this);
+};
+
+// This is the draw function, make sure to setup proper drawing environment, and more
+// importantly, make sure to _NOT_ change any state.
+MyGame.prototype.draw = function () {
+    // Step A: clear the canvas
+    gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+
+    // Step  B: Activate the drawing Camera
+    this.mCamera.setupViewProjection();
+
+    // Step  C: Activate the white shader to draw
+    this.mWhiteSq.draw(this.mCamera.getVPMatrix());
+
+    // Step  D: Activate the red shader to draw
+    this.mRedSq.draw(this.mCamera.getVPMatrix());
+};
+
+// The Update function, updates the application state. Make sure to _NOT_ draw
+// anything from this function!
+MyGame.prototype.update = function () {
+    // For this very simple game, let's move the white square and pulse the red
+
+    // Step A: move the white square
+    var whiteXform = this.mWhiteSq.getXform();
+    var deltaX = 0.05;
+    if (whiteXform.getXPos() > 30) // this is the right-bound of the window
+        whiteXform.setPosition(10, 60);
+    whiteXform.incXPosBy(deltaX);
+    whiteXform.incRotationByDegree(1);
+
+    // Step B: pulse the red square
+    var redXform = this.mRedSq.getXform();
+    if (redXform.getWidth() > 5)
+        redXform.setSize(2, 2);
+    redXform.incSizeBy(0.05);
+};
